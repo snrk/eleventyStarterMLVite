660255179e4fb92f7aecaa617ec77dda4ca65d82
snrk <buser.gabriel@gmail.com>
2024-02-19 14:55:34 +0100

    update footer adress

--- src/_data/footer.js
+++ src/_data/footer.js
@@ -2,8 +2,8 @@ module.exports = {
     mapUrl: "https://www.google.com/maps/place/E2i+expertises+immobili%C3%A8res+ind%C3%A9pendantes/@46.5730231,6.6054437,17z/data=!3m1!4b1!4m6!3m5!1s0x478c323e60b46bf3:0x960c4c7360889b3f!8m2!3d46.5730231!4d6.6054437!16s%2Fg%2F11btl_y0v7?entry=ttu",
     fr: {
         addressTitle: "Contact",
-        addressStreet: "Route de Neuchâtel",
-        addressNumber: "8",
+        addressStreet: "Chemin de la Source",
+        addressNumber: "27B",
         addressPostcode: "1032",
         addressCity: "Romanel-sur-Lausanne",
         directionsLabel: "Nous trouver",
@@ -16,8 +16,8 @@ module.exports = {
     },
     en: {
         addressTitle: "Contact",
-        addressStreet: "Route de Neuchâtel",
-        addressNumber: "8",
+        addressStreet: "Chemin de la Source",
+        addressNumber: "27B",
         addressPostcode: "1032",
         addressCity: "Romanel-sur-Lausanne",
         directionsLabel: "Find us",
